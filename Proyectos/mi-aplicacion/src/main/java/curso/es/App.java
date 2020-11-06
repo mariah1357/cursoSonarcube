d1c74f8d88a56e317474956727d1ab0a1c5cd162
Ubuntu <ubuntu@ip-172-31-1-132.eu-west-1.compute.internal>
2020-11-06 19:14:53 +0000

    Final del curso

---
+++ Proyectos/mi-aplicacion/src/main/java/curso/es/App.java
@@ -0,0 +1,16 @@
+package curso.es;
+
+/**
+ * Documentacion de la clase APP!
+ *
+ */
+public class App {
+    
+    public static int suma( int numero1, int numero2 ) {
+        return numero1 + numero2 ;
+    }
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
